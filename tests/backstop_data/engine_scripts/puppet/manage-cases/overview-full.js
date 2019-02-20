4a48225327780e820990a2813bd974c7c2bd9cfa
René Olivo <minet.ws@gmail.com>
2019-02-20 20:05:41 +0000

    C51-393: Move full case details scenario logic to json file

--- tests/backstop_data/engine_scripts/puppet/manage-cases/overview-full.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = async (page, scenario, vp) => {
-  await require('./../manage-cases/select-case.js')(page, scenario, vp);
-
-  await page.click('.civicase__case-body_tab > li:nth-child(1) a');
-  await page.waitFor(300); // Wait for animation to complete
-};
