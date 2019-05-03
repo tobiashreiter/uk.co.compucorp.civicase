dd48a6adc7e786d6ad5aab79f19369800be795c9
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-05-03 18:55:07 +0100

    CATL-1057: Add temp table for when unique cases checkbox is checked.

--- CRM/Civicase/Form/Report/Case/CaseWithActivityPivot.php
+++ CRM/Civicase/Form/Report/Case/CaseWithActivityPivot.php
@@ -16,6 +16,7 @@ class CRM_Civicase_Form_Report_Case_CaseWithActivityPivot extends CRM_Civicase_F
   protected $isPivot = TRUE;
   protected $_noFields = TRUE;
   protected $_potentialCriteria = [];
+  protected $tempCaseActivityTableName = '';
 
 
   public function __construct() {
@@ -41,6 +42,7 @@ class CRM_Civicase_Form_Report_Case_CaseWithActivityPivot extends CRM_Civicase_F
 
     $this->_tagFilter = TRUE;
     $this->_groupFilter = TRUE;
+    $this->addResultsTab();
     parent::__construct();
   }
 
@@ -54,6 +56,56 @@ class CRM_Civicase_Form_Report_Case_CaseWithActivityPivot extends CRM_Civicase_F
     return [
       'contact_from_case',
       'activity_from_case',
+    ];
+  }
+
+
+  /**
+   * SQl query to join the to activity table from the case table
+   */
+  function joinActivityFromCase() {
+    $caseActivityTable = $this->_caseActivityTable;
+    if (!empty($this->_params['unique_cases'])) {
+      $this->generateTempCaseActivityTable();
+      $caseActivityTable = $this->tempCaseActivityTableName;
+    }
+
+    $this->_from .= "
+      LEFT JOIN {$caseActivityTable} cca ON cca.case_id = {$this->_aliases['civicrm_case']}.id
+      LEFT JOIN civicrm_activity {$this->_aliases['civicrm_activity']} ON {$this->_aliases['civicrm_activity']}.id = cca.activity_id";
+  }
+
+  /**
+   * Temp table for unique case IDs with activity
+   */
+  protected function generateTempCaseActivityTable() {
+    $tempTable = 'civicrm_unique_case_activity' . date('d_H_I') . rand(1, 10000);
+    $sql = "CREATE {$this->_temporary} TABLE $tempTable
+    (`case_id` INT(10) UNSIGNED NULL DEFAULT '0',
+    `activity_id` INT(10) UNSIGNED NULL DEFAULT '0',
+    INDEX `case_id` (`case_id`),
+    INDEX `activity_id` (`activity_id`)
+    )
+    COLLATE='utf8_unicode_ci'
+    ENGINE=HEAP;";
+    CRM_Core_DAO::executeQuery($sql);
+    $sql = "
+      INSERT INTO $tempTable
+      SELECT DISTINCT case_id, activity_id FROM civicrm_case_activity GROUP BY case_id;";
+    CRM_Core_DAO::executeQuery($sql);
+    $this->tempCaseActivityTableName = $tempTable;
+  }
+
+  /**
+   * Add the results tab to the tabs list. Also add the unique_cases checkbox to return
+   * unique case Ids when checked.
+   */
+  protected function addResultsTab() {
+    $this->add('advcheckbox', 'unique_cases', ts('Include Only Unique Cases?'));
+    $this->tabs['Results'] = [
+      'title' => ts('Results'),
+      'tpl' => 'Results',
+      'div_label' => 'set-results',
     ];
   }
 }
---
+++ templates/CRM/Report/Form/Tabs/Results.tpl
@@ -0,0 +1,12 @@
+<div id="report-tab-set-results" class="civireport-criteria">
+  <table class="report-layout">
+    <tr class="crm-report crm-report-criteria-results">
+      <td>
+        <div id='crm-custom_fields'>
+          <label>{ts}{$form.unique_cases.label}{/ts}</label>
+          {$form.unique_cases.html}
+        </div>
+      </td>
+    </tr>
+  </table>
+</div>
