4ed43148b3926010898379b4a403ec6e8c2b98da
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-07-15 17:45:18 +0100

    RSE-227: Add the case category to the list of object that a custom field can extend.

---
+++ CRM/Civicase/Setup/AddCaseCategoryCgExtendsValue.php
@@ -0,0 +1,23 @@
+<?php
+
+
+class CRM_Civicase_Setup_AddCaseCategoryCgExtendsValue {
+
+  /**
+   * Add the CaseCategory as a valid Entity that a custom group can extend.
+   *
+   * @return bool
+   */
+  public function apply() {
+    $caseCategoryLabel = 'CaseCategory';
+
+    CRM_Core_BAO_OptionValue::ensureOptionValueExists([
+      'option_group_id' => 'cg_extend_objects',
+      'name' => 'civicrm_case_type',
+      'label' => $caseCategoryLabel,
+      'value' => $caseCategoryLabel,
+      'is_active' => TRUE,
+      'is_reserved' => TRUE,
+    ]);
+  }
+}
--- CRM/Civicase/Upgrader.php
+++ CRM/Civicase/Upgrader.php
@@ -2,6 +2,7 @@
 
 use CRM_Civicase_Setup_CaseTypeCategorySupport as CaseTypeCategorySupport;
 use CRM_Civicase_Setup_CreateCasesOptionValue as CreateCasesOptionValue;
+use CRM_Civicase_Setup_AddCaseCategoryCgExtendsValue as AddCaseCategoryCgExtendsValue;
 
 /**
  * Collection of upgrade steps.
@@ -63,6 +64,7 @@ class CRM_Civicase_Upgrader extends CRM_Civicase_Upgrader_Base {
     $steps = [
       new CaseTypeCategorySupport(),
       new CreateCasesOptionValue(),
+      new AddCaseCategoryCgExtendsValue(),
     ];
     foreach ($steps as $step) {
       $step->apply();
