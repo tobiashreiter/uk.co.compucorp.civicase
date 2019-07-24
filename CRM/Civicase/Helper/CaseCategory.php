d4fc8fcccbb961605c9513665f3951128f72c007
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-07-24 19:16:34 +0100

    RSE-217: Add the CaseCategory helper.

---
+++ CRM/Civicase/Helper/CaseCategory.php
@@ -0,0 +1,40 @@
+<?php
+
+use CRM_Case_BAO_CaseType as CaseType;
+
+/**
+ * CRM_Civicase_Helper_CaseCategory class.
+ */
+class CRM_Civicase_Helper_CaseCategory {
+
+  /**
+   * Returns the case category name for the case Id.
+   *
+   * @param int $caseId
+   *   The Case ID.
+   *
+   * @return string|null
+   *   The Case Category Name.
+   */
+  public static function getCategoryName($caseId) {
+    $caseTypeCategories = CaseType::buildOptions('case_type_category');
+
+    $result = civicrm_api3('Case', 'getsingle', [
+      'id' => $caseId,
+      'return' => ['case_type_id'],
+      'api.CaseType.getsingle' => [
+        'id' => '$value.case_type_id',
+        'return' => ['case_type_category'],
+      ],
+    ]);
+
+    if (!empty($result['api.CaseType.getsingle']['case_type_category'])) {
+      $caseCategoryId = $result['api.CaseType.getsingle']['case_type_category'];
+
+      return $caseTypeCategories[$caseCategoryId];
+    }
+
+    return NULL;
+  }
+
+}
