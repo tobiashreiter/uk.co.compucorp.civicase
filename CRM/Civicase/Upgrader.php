513bf3f60f827423283483900146a1a331ee5e71
Tunbola Ogunwande <tunbolawande@yahoo.com>
2019-08-14 16:05:03 +0100

    RSE-216: Add Setup for fetching case types for case category on Custom group extends.

--- CRM/Civicase/Upgrader.php
+++ CRM/Civicase/Upgrader.php
@@ -2,6 +2,7 @@
 
 use CRM_Civicase_Setup_CaseTypeCategorySupport as CaseTypeCategorySupport;
 use CRM_Civicase_Setup_CreateCasesOptionValue as CreateCasesOptionValue;
+use CRM_Civicase_Setup_AddCaseTypesForCustomGroupExtends as AddCaseTypesForCustomGroupExtends;
 
 /**
  * Collection of upgrade steps.
@@ -63,6 +64,7 @@ class CRM_Civicase_Upgrader extends CRM_Civicase_Upgrader_Base {
     $steps = [
       new CaseTypeCategorySupport(),
       new CreateCasesOptionValue(),
+      new AddCaseTypesForCustomGroupExtends(),
     ];
     foreach ($steps as $step) {
       $step->apply();
