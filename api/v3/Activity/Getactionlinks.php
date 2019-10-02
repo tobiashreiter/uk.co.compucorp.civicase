f96f00c6b1cdbca9795e5b88b401934ce4f41a60
Debarshi Bhaumik <deb1990@gmail.com>
2019-10-02 13:01:33 +0530

    CATL-1145: Add error handling to getactionlinks api

--- api/v3/Activity/Getactionlinks.php
+++ api/v3/Activity/Getactionlinks.php
@@ -1,16 +1,19 @@
 <?php
 
+/**
+ * @file
+ * Activity.getactionlinks file.
+ */
+
 const ACTIONS_DEFINED_BY_CIVICASE = ['Delete', 'File on Case', 'Edit'];
 
 /**
- * Activity.getactionlinks API specification (optional)
- * This is used for documentation and validation.
+ * Activity.getactionlinks API specification (optional).
  *
- * @param array $spec description of fields supported by this API call
- * @return void
- * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ * @param array $spec
+ *   description of fields supported by this API call.
  */
-function _civicrm_api3_activity_getactionlinks_spec(&$spec) {
+function _civicrm_api3_activity_getactionlinks_spec(array &$spec) {
   $spec['activity_type_id']['api.required'] = 1;
   $spec['source_record_id']['api.required'] = 1;
   $spec['activity_id']['api.required'] = 1;
@@ -18,17 +21,21 @@ function _civicrm_api3_activity_getactionlinks_spec(&$spec) {
 }
 
 /**
- * Activity.getactionlinks API
+ * Activity.getactionlinks API.
+ *
  * This API returns the activity action links for an activity record.
  * Basically it adds the action links added by core plus the action links
  * added by various extensions via hooks. Action links differ per activity
- * as some logic may determine which links are available for a particular activity.
+ * as some logic may determine which links are available for a particular
+ * activity.
  *
  * @param array $params
+ *   Params.
  *
  * @return array
+ *   Links
  */
-function civicrm_api3_activity_getactionlinks($params) {
+function civicrm_api3_activity_getactionlinks(array $params) {
   return _civicrm_api3_activity_getActivityActionLinks($params);
 }
 
@@ -36,16 +43,23 @@ function civicrm_api3_activity_getactionlinks($params) {
  * Returns activity links for the activity ID in the params.
  *
  * @param array $params
+ *   Params.
  *
  * @return array
+ *   Links.
  */
-function _civicrm_api3_activity_getActivityActionLinks($params) {
-  $actionLinks = CRM_Activity_Selector_Activity::actionLinks(
-    CRM_Utils_Array::value('activity_type_id', $params),
-    CRM_Utils_Array::value('source_record_id', $params),
-    FALSE,
-    CRM_Utils_Array::value('activity_id', $params)
-  );
+function _civicrm_api3_activity_getActivityActionLinks(array $params) {
+  try {
+    $actionLinks = CRM_Activity_Selector_Activity::actionLinks(
+      CRM_Utils_Array::value('activity_type_id', $params),
+      CRM_Utils_Array::value('source_record_id', $params),
+      FALSE,
+      CRM_Utils_Array::value('activity_id', $params)
+    );
+  }
+  catch (Exception $e) {
+    return [];
+  }
 
   $actionMask = array_sum(array_keys($actionLinks));
 
@@ -62,7 +76,7 @@ function _civicrm_api3_activity_getActivityActionLinks($params) {
     'caseid' => CRM_Utils_Array::value('case_id', $params),
   ];
 
-  //Invoke hook links for activity tab rows.
+  // Invoke hook links for activity tab rows.
   CRM_Utils_Hook::links(
     'activity.tab.row',
     'Activity',
@@ -72,26 +86,27 @@ function _civicrm_api3_activity_getActivityActionLinks($params) {
     $values
   );
 
- return  _civicrm_api3_activity_GetActionLinks_processLinks($seqLinks);
+  return _civicrm_api3_activity_GetActionLinks_processLinks($seqLinks);
 }
 
 /**
- * Process activity links
+ * Process activity links.
  *
  * @param array $activityActionLinks
+ *   Activity Action Links.
  *
  * @return array
+ *   Activity Action Links.
  */
-function _civicrm_api3_activity_GetActionLinks_processLinks($activityActionLinks) {
-  foreach($activityActionLinks as $id => $link) {
-
-    //remove action links already added by civicase
-    if (in_array($link['name'], ACTIONS_DEFINED_BY_CIVICASE )) {
+function _civicrm_api3_activity_GetActionLinks_processLinks(array $activityActionLinks) {
+  foreach ($activityActionLinks as $id => $link) {
+    // Remove action links already added by civicase.
+    if (in_array($link['name'], ACTIONS_DEFINED_BY_CIVICASE)) {
       unset($activityActionLinks[$id]);
       continue;
     }
 
-    //format link URL
+    // Format link URL.
     if (isset($link['qs']) && !CRM_Utils_System::isNull($link['qs'])) {
       $urlPath = CRM_Utils_System::url(CRM_Core_Action::replace($link['url'], $values),
         CRM_Core_Action::replace($link['qs'], $values), FALSE, NULL, TRUE
@@ -103,7 +118,7 @@ function _civicrm_api3_activity_GetActionLinks_processLinks($activityActionLinks
 
     $activityActionLinks[$id]['url'] = $urlPath;
 
-    // Add link classes
+    // Add link classes.
     $classes = 'action-item';
     if (isset($link['ref'])) {
       $classes .= ' ' . strtolower($link['ref']);
