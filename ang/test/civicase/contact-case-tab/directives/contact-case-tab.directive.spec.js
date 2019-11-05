15928819fdbfec626d0a5fb1f9b6d5e83bc8f46e
Rene Olivo <rene.olivo.do@gmail.com>
2019-11-05 16:30:06 +0000

    CATL-1142: Convert contact case tab spec file to ES6

--- ang/test/civicase/contact-case-tab/directives/contact-case-tab.directive.spec.js
+++ ang/test/civicase/contact-case-tab/directives/contact-case-tab.directive.spec.js
@@ -1,29 +1,29 @@
 /* eslint-env jasmine */
 
-(function (_) {
-  describe('Contact Case Tab', function () {
+((_) => {
+  describe('Contact Case Tab', () => {
     var $controller, $rootScope, $scope, mockContactId, mockContactService;
 
-    beforeEach(module('civicase', function ($provide) {
+    beforeEach(module('civicase', ($provide) => {
       mockContactService = jasmine.createSpyObj('Contact', ['getContactIDFromUrl']);
 
       $provide.value('Contact', mockContactService);
     }));
 
-    beforeEach(inject(function (_$controller_, _$rootScope_) {
+    beforeEach(inject((_$controller_, _$rootScope_) => {
       $controller = _$controller_;
       $rootScope = _$rootScope_;
     }));
 
-    beforeEach(function () {
+    beforeEach(() => {
       mockContactId = _.uniqueId();
 
       mockContactService.getContactIDFromUrl.and.returnValue(mockContactId);
       initController();
     });
 
-    describe('on init', function () {
-      it('stores the contact id extracted from the URL', function () {
+    describe('on init', () => {
+      it('stores the contact id extracted from the URL', () => {
         expect($scope.contactId).toBe(mockContactId);
       });
     });
