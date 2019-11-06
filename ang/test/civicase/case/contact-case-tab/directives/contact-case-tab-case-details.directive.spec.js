bec3b342ddb66a910fff831ab160bab4c4c498d8
Rene Olivo <rene.olivo.do@gmail.com>
2019-11-06 22:43:21 +0000

    CATL-1128: Add status id when opening case details page

--- ang/test/civicase/case/contact-case-tab/directives/contact-case-tab-case-details.directive.spec.js
+++ ang/test/civicase/case/contact-case-tab/directives/contact-case-tab-case-details.directive.spec.js
@@ -1,1 +1,40 @@
-// Todo write unit tests for ContactCaseTabCaseDetails.js
+/* eslint-env jasmine */
+
+(() => {
+  describe('Contact Case Tab Case Details', () => {
+    let $controller, $rootScope, $scope, mockCase;
+
+    beforeEach(module('civicase', 'civicase.data'));
+
+    beforeEach(inject((_$controller_, _$rootScope_, _CasesData_, _crmApi_) => {
+      $controller = _$controller_;
+      $rootScope = _$rootScope_;
+      mockCase = _CasesData_.get().values[0];
+
+      initController();
+    }));
+
+    describe('when requesting the case details page URL', () => {
+      let expectedUrl, returnedUrl;
+
+      beforeEach(() => {
+        expectedUrl = '/civicrm/case/a/#/case/list' +
+          `?caseId=${mockCase.id}&cf={"status_id":[${mockCase.status_id}]}`;
+        returnedUrl = $scope.getCaseDetailsUrl(mockCase);
+      });
+
+      it('returns the case details page url for the given case', () => {
+        expect(returnedUrl).toEqual(expectedUrl);
+      });
+    });
+
+    /**
+     * Initializes the contact case tab case details controller.
+     */
+    function initController () {
+      $scope = $rootScope.$new();
+
+      $controller('CivicaseContactCaseTabCaseDetailsController', { $scope: $scope });
+    }
+  });
+})();
