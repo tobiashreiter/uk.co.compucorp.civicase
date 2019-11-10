b72667267b54031cb64bc25f96f386f82f53713b
Rene Olivo <rene.olivo.do@gmail.com>
2019-11-10 17:34:03 +0000

    CATL-1128: Add get labels for values method to case status service

--- ang/civicase-base/services/case-status.service.js
+++ ang/civicase-base/services/case-status.service.js
@@ -7,8 +7,32 @@
    * Case Status Service
    */
   function CaseStatus () {
-    this.getAll = function () {
-      return CRM['civicase-base'].caseStatuses;
-    };
+    var caseStatuses = CRM['civicase-base'].caseStatuses;
+
+    this.getAll = getAll;
+    this.getLabelsForValues = getLabelsForValues;
+
+    /**
+     * @returns {object[]} a list of all the case statuses.
+     */
+    function getAll () {
+      return caseStatuses;
+    }
+
+    /**
+     * Returns the labels for the given case status values.
+     *
+     * @param {string[]} statusValues a list of case status values.
+     * @returns {string[]} a list of case status labels.
+     */
+    function getLabelsForValues (statusValues) {
+      return _.map(statusValues, function (statusValue) {
+        var caseStatus = _.find(caseStatuses, function (caseStatus) {
+          return caseStatus.value === statusValue;
+        });
+
+        return caseStatus.label;
+      });
+    }
   }
 })(angular, CRM.$, CRM._, CRM);
