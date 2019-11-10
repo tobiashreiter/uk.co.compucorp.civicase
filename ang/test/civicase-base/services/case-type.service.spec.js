b1c9306b3ff15aab3bfa285f503929e3a270d3bd
Rene Olivo <rene.olivo.do@gmail.com>
2019-11-10 17:55:57 +0000

    CATL-1128: Add get title for names method to case type service

---
+++ ang/test/civicase-base/services/case-type.service.spec.js
@@ -0,0 +1,44 @@
+/* eslint-env jasmine */
+
+(() => {
+  describe('Case Type', () => {
+    let CaseType, CaseTypesData;
+
+    beforeEach(module('civicase', 'civicase.data'));
+
+    beforeEach(inject((_CaseType_, _CaseTypes_) => {
+      CaseType = _CaseType_;
+      CaseTypesData = _CaseTypes_.get();
+    }));
+
+    describe('when getting all case types', () => {
+      let returnedCaseTypes;
+
+      beforeEach(() => {
+        returnedCaseTypes = CaseType.getAll();
+      });
+
+      it('returns all the case types', () => {
+        expect(returnedCaseTypes).toEqual(CaseTypesData);
+      });
+    });
+
+    describe('when getting the titles for case types using their name', () => {
+      let returnedTitles;
+
+      beforeEach(() => {
+        returnedTitles = CaseType.getTitlesForNames([
+          'housing_support',
+          'adult_day_care_referral'
+        ]);
+      });
+
+      it('returns the title for the given case types', () => {
+        expect(returnedTitles).toEqual([
+          'Housing Support',
+          'Adult Day Care Referral'
+        ]);
+      });
+    });
+  });
+})();
