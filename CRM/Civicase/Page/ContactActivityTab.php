8a88a6490ef8fb0847dcdbdb9c603e8f4d9449b7
Debarshi Bhaumik <deb1990@gmail.com>
2019-11-20 14:00:00 +0000

    CPS-31: Change Activity Button color

--- CRM/Civicase/Page/ContactActivityTab.php
+++ CRM/Civicase/Page/ContactActivityTab.php
@@ -3,7 +3,7 @@
 /**
  * Class CRM_Civicase_Page_ContactActivityTab.
  *
- * Implement the Angular version of the tab "View Contact => Activities".
+ * Implements the Angular version of the tab "View Contact => Activities".
  */
 class CRM_Civicase_Page_ContactActivityTab extends CRM_Core_Page {
 
