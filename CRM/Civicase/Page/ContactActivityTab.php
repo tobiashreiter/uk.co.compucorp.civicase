114bc34afd9fe6a6fb720b3ee4cb4554ce40df66
Debarshi Bhaumik <deb1990@gmail.com>
2019-11-20 13:00:00 +0000

    CPS-31: Add Activity Button

--- CRM/Civicase/Page/ContactActivityTab.php
+++ CRM/Civicase/Page/ContactActivityTab.php
@@ -3,8 +3,43 @@
 /**
  * Class CRM_Civicase_Page_ContactActivityTab.
  *
- * Implements the Angular version of the tab "View Contact => Activities".
+ * Implement the Angular version of the tab "View Contact => Activities".
  */
 class CRM_Civicase_Page_ContactActivityTab extends CRM_Core_Page {
+
+  /**
+   * Run function of the class.
+   */
+  public function run() {
+    $this->preProcess();
+
+    return parent::run();
+  }
+
+  /**
+   * Pre process function.
+   */
+  public function preProcess() {
+    $this->_contactId = CRM_Utils_Request::retrieve('cid', 'Positive', $this, TRUE);
+    $this->assign('contactId', $this->_contactId);
+    // FIXME: need to fix this conflict.
+    $this->assign('contactID', $this->_contactId);
+
+    // Check logged in url permission.
+    CRM_Contact_Page_View::checkUserPermission($this);
+
+    $this->_action = CRM_Utils_Request::retrieve('action', 'String', $this, FALSE, 'browse');
+    $this->assign('action', $this->_action);
+
+    // Also create the form element for the activity links box.
+    $controller = new CRM_Core_Controller_Simple(
+      'CRM_Activity_Form_ActivityLinks',
+      ts('Activity Links'),
+      NULL,
+      FALSE, FALSE, TRUE
+    );
+    $controller->setEmbedded(TRUE);
+    $controller->run();
+  }
 
 }
--- templates/CRM/Civicase/Page/ContactActivityTab.tpl
+++ templates/CRM/Civicase/Page/ContactActivityTab.tpl
@@ -1,4 +1,9 @@
-<div id="civicaseActivitiesTab" >
+<div id="civicaseActivitiesTab">
+  {if $action EQ 16 and $permission EQ 'edit' and !$addAssigneeContact and !$addTargetContact}
+    <div class="civicase__contact-activity-tab__add">
+      {include file="CRM/Activity/Form/ActivityLinks.tpl" as_select=true}
+    </div>
+  {/if}
   <div class="container" ng-view></div>
 </div>
 {literal}
