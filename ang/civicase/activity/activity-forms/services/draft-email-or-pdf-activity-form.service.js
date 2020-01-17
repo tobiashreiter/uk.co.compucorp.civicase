6009b155047fb930eba8064cd606b4028fc1c6e6
Swastik Pareek <swastik1608@gmail.com>
2020-01-17 13:39:59 +0000

    CATL-1113: Fixes after rebasing from master

--- ang/civicase/activity/activity-forms/services/draft-email-or-pdf-activity-form.service.js
+++ ang/civicase/activity/activity-forms/services/draft-email-or-pdf-activity-form.service.js
@@ -17,13 +17,30 @@
      * @returns {string} the form URL for activities that are either email drafts or PDF drafts.
      */
     function getActivityFormUrl (activity) {
+      var caseClientsIds = getCaseClientsIds(activity).join(',');
+
       return getCrmUrl('civicrm/activity/email/add', {
         action: 'add',
         caseId: activity.case_id,
-        context: 'standalone',
+        cid: caseClientsIds,
         draft_id: activity.id,
         reset: '1'
       });
+    }
+
+    /**
+     * @param {object} activity an activity object.
+     * @returns {Array} of all client ids.
+     */
+    function getCaseClientsIds (activity) {
+      return _.chain(activity['case_id.contacts'])
+        .filter(function (contact) {
+          return contact.role === 'Client';
+        })
+        .map(function (client) {
+          return client.contact_id;
+        })
+        .value();
     }
   }
 })(CRM._, angular, CRM.url);
