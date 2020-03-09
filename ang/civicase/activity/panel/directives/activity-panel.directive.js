e5b43ccba329860bb0c1e15f2179d280d5b682e2
Patrick Figel <pfigel@greenpeace.org>
2020-03-09 15:30:02 +0100

    Fix activity not loading when status grouping is undefined
    This fixes an issue where activity details may not load if an activity
    status has no grouping:
    
    TypeError: Cannot read property 'split' of undefined
      at setAllowedActivityStatuses

--- ang/civicase/activity/panel/directives/activity-panel.directive.js
+++ ang/civicase/activity/panel/directives/activity-panel.directive.js
@@ -172,8 +172,9 @@
       $scope.allowedActivityStatuses = {};
 
       _.each(ActivityStatus.getAll(), function (activityStatus, activityStatusID) {
-        var ifStatusIsInSameCategory = _.intersection($scope.activity.category, activityStatus.grouping.split(',')).length > 0;
-        var ifStatusIsInNoneCategory = $scope.activity.category.length === 0 && activityStatus.grouping.split(',').indexOf('none') !== -1;
+        var statusGrouping = activityStatus.grouping ? activityStatus.grouping.split(',') : [];
+        var ifStatusIsInSameCategory = _.intersection($scope.activity.category, statusGrouping).length > 0;
+        var ifStatusIsInNoneCategory = $scope.activity.category.length === 0 && statusGrouping.indexOf('none') !== -1;
 
         if (ifStatusIsInSameCategory || ifStatusIsInNoneCategory) {
           $scope.allowedActivityStatuses[activityStatusID] = activityStatus;
