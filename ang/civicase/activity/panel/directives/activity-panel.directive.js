4277d2aead6d8032cd36ac0ee4b0e46649ae7207
Debarshi Bhaumik <deb1990@gmail.com>
2020-03-10 12:12:30 +0000

    CPS-106: Show activity status if no categories are selected

--- ang/civicase/activity/panel/directives/activity-panel.directive.js
+++ ang/civicase/activity/panel/directives/activity-panel.directive.js
@@ -175,8 +175,9 @@
         var statusGrouping = activityStatus.grouping ? activityStatus.grouping.split(',') : [];
         var ifStatusIsInSameCategory = _.intersection($scope.activity.category, statusGrouping).length > 0;
         var ifStatusIsInNoneCategory = $scope.activity.category.length === 0 && statusGrouping.indexOf('none') !== -1;
+        var ifStatusIsForAllCategories = statusGrouping.length === 0;
 
-        if (ifStatusIsInSameCategory || ifStatusIsInNoneCategory) {
+        if (ifStatusIsInSameCategory || ifStatusIsInNoneCategory || ifStatusIsForAllCategories) {
           $scope.allowedActivityStatuses[activityStatusID] = activityStatus;
         }
       });
