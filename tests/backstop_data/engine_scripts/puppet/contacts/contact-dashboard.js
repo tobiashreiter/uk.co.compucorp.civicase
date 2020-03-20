1b54a2c13a3511f5b0976afc7626803fd1fe69cc
Swastik Pareek <swastik1608@gmail.com>
2020-03-20 09:10:34 +0000

    CPS-136: Tweak contacts dashboard js and associated readme content

--- tests/backstop_data/engine_scripts/puppet/contacts/contact-dashboard.js
+++ tests/backstop_data/engine_scripts/puppet/contacts/contact-dashboard.js
@@ -5,7 +5,7 @@ const Utility = require('./../utility.js');
 module.exports = async (page, scenario, vp) => {
   const utility = new Utility(page, scenario, vp);
 
-  await page.type('#sort_name', 'Betty Adams');
+  await page.type('#sort_name', 'Betty');
   await utility.clickAndWaitForNavigation('#_qf_Basic_refresh');
   await utility.clickAndWaitForNavigation('.crm-search-results a[title="View Contact Details"]');
 };
