8b5aefd8ebfd7ccb75cb00241d6f921f5d56f592
Debarshi Bhaumik <deb1990@gmail.com>
2020-04-09 11:54:16 +0100

    RSE-1035: Filter webforms on the front ned

--- ang/civicase/case/actions/services/go-to-webform-case-action.service.js
+++ ang/civicase/case/actions/services/go-to-webform-case-action.service.js
@@ -3,15 +3,22 @@
 
   module.service('GoToWebformCaseAction', GoToWebformCaseAction);
 
+  /**
+   * @param {object} $window browsers window object
+   */
   function GoToWebformCaseAction ($window) {
+    this.doAction = doAction;
+    this.isActionAllowed = isActionAllowed;
+    this.isWebformContainsCaseTypeId = isWebformContainsCaseTypeId;
+
     /**
      * Click event handler for the Action
      *
-     * @param {Array} cases
-     * @param {Object} action
-     * @param {Function} callbackFn
+     * @param {Array} cases cases object
+     * @param {object} action action object
+     * @param {Function} callbackFn callback function
      */
-    this.doAction = function (cases, action, callbackFn) {
+    function doAction (cases, action, callbackFn) {
       var window;
       var urlObject = { case1: cases[0].id };
 
@@ -21,6 +28,28 @@
 
       window = $window.open(CRM.url(action.path, urlObject), '_blank');
       window.focus();
-    };
+    }
+
+    /**
+     * Check if action is allowed.
+     *
+     * @param {object} action - action data.
+     * @param {object} cases - cases.
+     * @param {object} attributes - item attributes.
+     *
+     * @returns {boolean} - true if action is allowed, false otherwise.
+     */
+    function isActionAllowed (action, cases, attributes) {
+      return isWebformContainsCaseTypeId(action, cases[0].case_type_id);
+    }
+
+    /**
+     * @param {object} webform webform action object
+     * @param {string} caseTypeID case type id
+     * @returns {boolean} if webform contains sent case type id
+     */
+    function isWebformContainsCaseTypeId (webform, caseTypeID) {
+      return webform.case_type_ids.indexOf(caseTypeID) !== -1;
+    }
   }
 })(angular, CRM.$, CRM._);
