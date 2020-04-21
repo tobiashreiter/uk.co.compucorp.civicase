3f24b75829c6fe2bf19a9563380fdfbef3e19973
Debarshi Bhaumik <deb1990@gmail.com>
2020-04-21 11:21:04 +0100

    RSE-1027: Add unit tests

--- ang/test/mocks/services/crm-api.factory.mock.js
+++ ang/test/mocks/services/crm-api.factory.mock.js
@@ -1,15 +1,11 @@
 /* eslint-env jasmine */
 
 (function () {
-  var crmUtilModule = angular.module('crmUtil');
-  var civicaseBaseModule = angular.module('civicase-base');
+  var module = angular.module('crmUtil');
 
-  var mockCrmApi = ['$q', function mockCrmApi ($q) {
+  module.factory('crmApi', ['$q', function ($q) {
     var crmApi = jasmine.createSpy('crmApi');
     crmApi.and.returnValue($q.resolve());
     return crmApi;
-  }];
-
-  crmUtilModule.factory('crmApi', mockCrmApi);
-  civicaseBaseModule.factory('crmApi', mockCrmApi);
+  }]);
 })();
