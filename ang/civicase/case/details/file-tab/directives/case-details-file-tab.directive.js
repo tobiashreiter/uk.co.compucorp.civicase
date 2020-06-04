16778bdde58a90f41aa6c27fd5c9c81ed77279e5
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2020-06-04 11:30:52 +0100

    CATL-1393: Files tab Bulk Action UI and select functionality

--- ang/civicase/case/details/file-tab/directives/case-details-file-tab.directive.js
+++ ang/civicase/case/details/file-tab/directives/case-details-file-tab.directive.js
@@ -11,15 +11,145 @@
       },
       controller: civicaseCaseDetailsFileTabController
     };
+  });
+
+  module.controller('civicaseCaseDetailsFileTab', civicaseCaseDetailsFileTabController);
+
+  /**
+   * Controller function for the directive
+   *
+   * @param {object} $scope controllers scope object
+   * @param {object} BulkActions bulk actions service
+   * @param {object} civicaseCrmApi service to call civicrm backend
+   */
+  function civicaseCaseDetailsFileTabController ($scope, BulkActions, civicaseCrmApi) {
+    $scope.ts = CRM.ts('civicase');
+    $scope.bulkAllowed = BulkActions.isAllowed();
+    $scope.isSelectAll = false;
+    $scope.selectedActivities = [];
+    $scope.totalCount = 0;
+
+    $scope.findActivityById = findActivityById;
+    $scope.toggleSelected = toggleSelected;
+    $scope.refresh = refresh;
+
+    (function init () {
+      $scope.$watchCollection('fileLists.result', fileListsWatcher);
+      $scope.$on('civicase::bulk-actions::bulk-selections', bulkSelectionsListener);
+    }());
 
     /**
-     * Controller function for the directive
+     * Bulk Selection Event Listener
      *
-     * @param {Object} $scope
+     * @param {object} event event
+     * @param {string} condition condition
      */
-    function civicaseCaseDetailsFileTabController ($scope, BulkActions) {
-      $scope.ts = CRM.ts('civicase');
-      $scope.bulkAllowed = BulkActions.isAllowed();
+    function bulkSelectionsListener (event, condition) {
+      if (condition === 'none') {
+        deselectAllActivities();
+      } else if (condition === 'visible') {
+        selectDisplayedActivities();
+      } else if (condition === 'all') {
+        selectEveryActivity();
+      }
     }
-  });
+
+    /**
+     * Deselection of all activities
+     */
+    function deselectAllActivities () {
+      $scope.isSelectAll = false;
+      $scope.selectedActivities = [];
+    }
+
+    /**
+     * Watcher function for fileLists.result collection
+     *
+     * @param {object} response response
+     */
+    function fileListsWatcher (response) {
+      if (!response) {
+        return;
+      }
+
+      // prettier html
+      $scope.values = response.values;
+      $scope.xref = response.xref;
+      // Pre-sorting: (a) cast to array and (b) ensure stable check of isSameDate()
+      $scope.activities = response.xref ? _.sortBy(response.xref.activity, 'activity_date_time').reverse() : [];
+      $scope.totalCount = $scope.activities.length;
+      $scope.filesByAct = {};
+      _.each(response.values, function (match) {
+        if (!$scope.filesByAct[match.activity_id]) {
+          $scope.filesByAct[match.activity_id] = [];
+        }
+        $scope.filesByAct[match.activity_id].push(response.xref.file[match.id]);
+      });
+    }
+
+    /**
+     * Find activity by given ID
+     *
+     * @param {Array} searchIn - array of activities to search into
+     * @param {number/string} activityID activityID
+     * @returns {object} activity
+     */
+    function findActivityById (searchIn, activityID) {
+      return _.find(searchIn, { id: activityID });
+    }
+
+    /**
+     * Refreshes the UI state after updating the db from the api calls
+     *
+     * @param {Array} apiCalls list of api calls
+     */
+    function refresh (apiCalls) {
+      if (!_.isArray(apiCalls)) apiCalls = [];
+
+      civicaseCrmApi(apiCalls, true).then(function (result) {
+        $scope.fileLists.refresh();
+      });
+    }
+
+    /**
+     * Select All visible data.
+     */
+    function selectDisplayedActivities () {
+      $scope.isSelectAll = false;
+      var isCurrentActivityInSelectedCases;
+
+      _.each($scope.activities, function (activity) {
+        isCurrentActivityInSelectedCases = $scope.findActivityById($scope.selectedActivities, activity.id);
+
+        if (!isCurrentActivityInSelectedCases) {
+          $scope.selectedActivities.push($scope.findActivityById($scope.activities, activity.id));
+        }
+      });
+    }
+
+    /**
+     * Select all Activity
+     */
+    function selectEveryActivity () {
+      deselectAllActivities();
+      $scope.isSelectAll = true;
+    }
+
+    /**
+     * Toggle Bulk Actions checkbox of the given activity
+     *
+     * @param {object} activity activity
+     */
+    function toggleSelected (activity) {
+      if ($scope.isSelectAll) {
+        deselectAllActivities();
+      }
+
+      if (!$scope.findActivityById($scope.selectedActivities, activity.id)) {
+        $scope.selectedActivities.push($scope.findActivityById($scope.activities, activity.id));
+      } else {
+        _.remove($scope.selectedActivities, { id: activity.id });
+      }
+    }
+  }
 })(angular, CRM.$, CRM._);
--- ang/civicase/case/details/file-tab/directives/file-list.directive.js
+++ ang/civicase/case/details/file-tab/directives/file-list.directive.js
@@ -4,60 +4,7 @@
   module.directive('civicaseFileList', function () {
     return {
       restrict: 'A',
-      templateUrl: '~/civicase/case/details/file-tab/directives/file-list.directive.html',
-      controller: civicaseFileListController,
-      scope: {
-        fileLists: '=civicaseFileList',
-        bulkAllowed: '='
-      }
+      templateUrl: '~/civicase/case/details/file-tab/directives/file-list.directive.html'
     };
   });
-
-  /**
-   * Controlle for File List
-   *
-   * @param {object} $scope scope object
-   * @param {object} civicaseCrmApi service to access civicrm api
-   */
-  function civicaseFileListController ($scope, civicaseCrmApi) {
-    $scope.ts = CRM.ts('civicase');
-
-    (function init () {
-      $scope.$watchCollection('fileLists.result', fileListsWatcher);
-    }());
-
-    /**
-     * Refreshes the UI state after updating the db from the api calls
-     *
-     * @param {Array} apiCalls list of api calls
-     */
-    $scope.refresh = function (apiCalls) {
-      if (!_.isArray(apiCalls)) apiCalls = [];
-
-      civicaseCrmApi(apiCalls, true).then(function (result) {
-        $scope.fileLists.refresh();
-      });
-    };
-
-    /**
-     * Watcher function for fileLists.result collection
-     *
-     * @param {object} response response object
-     */
-    function fileListsWatcher (response) {
-      // prettier html
-      $scope.values = response.values;
-      $scope.xref = response.xref;
-      // Pre-sorting: (a) cast to array and (b) ensure stable check of isSameDate()
-      $scope.activities = response.xref ? _.sortBy(response.xref.activity, 'activity_date_time').reverse() : [];
-
-      $scope.filesByAct = {};
-      _.each(response.values, function (match) {
-        if (!$scope.filesByAct[match.activity_id]) {
-          $scope.filesByAct[match.activity_id] = [];
-        }
-        $scope.filesByAct[match.activity_id].push(response.xref.file[match.id]);
-      });
-    }
-  }
 })(angular, CRM.$, CRM._);
