292b8561f1c3e4d9f634d7e4962fb9cf2aeb6981
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2020-06-05 09:15:43 +0100

    CATL-1393: Fix Lint issues

--- CRM/Civicase/Page/ActivityFiles.php
+++ CRM/Civicase/Page/ActivityFiles.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Class CRM_Civicase_Page_ActivityFiles.
+ *
+ * Handles downloading of attachments of activities.
+ */
 class CRM_Civicase_Page_ActivityFiles {
 
   /**
@@ -19,8 +24,10 @@ class CRM_Civicase_Page_ActivityFiles {
   }
 
   /**
-   * Returns the activity specified by the request. In case the request gives
-   * an invalid activity id it throws a 404 status code.
+   * Returns the activity specified by the request.
+   *
+   * In case the request gives an invalid activity id it
+   * throws a 404 status code.
    */
   private static function getActivityFromRequest() {
     $activityId = CRM_Utils_Array::value('activity_id', $_GET);
@@ -29,7 +36,7 @@ class CRM_Civicase_Page_ActivityFiles {
 
     $activityResult = civicrm_api3('Activity', 'get', [
       'id' => $activityId,
-      'return' => [ 'activity_type_id.label' ]
+      'return' => ['activity_type_id.label'],
     ]);
 
     if ($activityResult['count'] === 0) {
@@ -40,9 +47,10 @@ class CRM_Civicase_Page_ActivityFiles {
   }
 
   /**
-   * Validates that the activity id was provided. If not, it returns a 404 status code.
+   * Validates the provided activity id. If not, it returns a 404 status code.
    *
    * @param string|null $activityId
+   *   Activity ID.
    */
   private static function validateActivityId($activityId) {
     if (empty($activityId)) {
@@ -54,6 +62,7 @@ class CRM_Civicase_Page_ActivityFiles {
    * Throws a specific status code and closes the connection.
    *
    * @param int $statusCode
+   *   Status code.
    */
   private static function throwStatusCode($statusCode) {
     http_response_code($statusCode);
@@ -61,14 +70,17 @@ class CRM_Civicase_Page_ActivityFiles {
   }
 
   /**
-   * Given an activity, it returns the name for the zip file containing all of
-   * its files. Ex: Activity Open Case 123.zip
+   * Returns the name of the zipped file for the given activity.
+   *
+   * Ex: Activity Open Case 123.zip.
    *
    * @param array $activity
+   *   Activity.
    *
    * @return string
+   *   Zip File name.
    */
-  private static function getZipName($activity) {
+  private static function getZipName(array $activity) {
     $name = 'Activity ' . $activity['activity_type_id.label'] . ' ' . $activity['id'];
 
     return CRM_Utils_String::munge($name, ' ') . '.zip';
@@ -78,6 +90,7 @@ class CRM_Civicase_Page_ActivityFiles {
    * Returns the destination path for the zip file.
    *
    * @return string
+   *   Destination path.
    */
   private static function getDestinationPath() {
     $config = CRM_Core_Config::singleton();
@@ -89,8 +102,10 @@ class CRM_Civicase_Page_ActivityFiles {
    * Returns a list of file paths that are part of a given activity.
    *
    * @param int|string $activityId
+   *   Activity ID.
    *
    * @return array
+   *   Activity file paths.
    */
   private static function getActivityFilePaths($activityId) {
     $filePaths = [];
@@ -107,11 +122,14 @@ class CRM_Civicase_Page_ActivityFiles {
    * Creates a zip file at the given path and containing the given files.
    *
    * @param string $zipFullPath
+   *   Zip file path.
    * @param array $filePaths
+   *   Individual file paths.
    *
    * @return resource
+   *   Resource.
    */
-  private static function createZipFile($zipFullPath, $filePaths) {
+  private static function createZipFile($zipFullPath, array $filePaths) {
     $mode = ZipArchive::CREATE | ZipArchive::OVERWRITE;
     $zip = new ZipArchive();
     $zipName = basename($zipFullPath);
@@ -134,7 +152,9 @@ class CRM_Civicase_Page_ActivityFiles {
    * Setups the given zip file resource so it can be downloaded by the browser.
    *
    * @param string $zipName
+   *   Zip file name.
    * @param resource $zipFileResource
+   *   Zip File Resource.
    */
   private static function downloadZipFileResource($zipName, $zipFileResource) {
     CRM_Utils_System::download($zipName, 'application/zip', $fileResource);
