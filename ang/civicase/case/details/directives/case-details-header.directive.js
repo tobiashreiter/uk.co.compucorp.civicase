ac5f7a14415e4e360d58bafd9e4e6b50738dd416
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2020-06-10 22:26:36 +0100

    CATL-1400: Webforms dropdown in Case Details Header

--- ang/civicase/case/details/directives/case-details-header.directive.js
+++ ang/civicase/case/details/directives/case-details-header.directive.js
@@ -5,7 +5,51 @@
     return {
       replace: true,
       restrict: 'E',
-      templateUrl: '~/civicase/case/details/directives/case-details-header.directive.html'
+      templateUrl: '~/civicase/case/details/directives/case-details-header.directive.html',
+      controller: 'civicaseCaseDetailsHeaderController'
     };
   });
+
+  module.controller('civicaseCaseDetailsHeaderController', civicaseCaseDetailsHeaderController);
+
+  /**
+   *
+   * @param {*} $scope scope object of the controller
+   * @param {*} CaseActions case action service
+   * @param {*} WebformsCaseAction webform case action service
+   * @param {*} GoToWebformCaseAction go to webform case action service
+   */
+  function civicaseCaseDetailsHeaderController ($scope, CaseActions, WebformsCaseAction, GoToWebformCaseAction) {
+    $scope.webformsAction = CaseActions.findByActionName('Webforms');
+    $scope.isGoToWebformAllowed = isGoToWebformAllowed;
+    $scope.openWebform = openWebform;
+
+    (function init () {
+      $scope.$watch('item', itemWatcher);
+    })();
+
+    /**
+     * @param {object} action action object
+     */
+    function openWebform (action) {
+      GoToWebformCaseAction.doAction([$scope.item], action);
+    }
+
+    /**
+     * @param {object} action action object
+     * @returns {object} is go to webform action allowed for the sent action
+     */
+    function isGoToWebformAllowed (action) {
+      return GoToWebformCaseAction.isActionAllowed(action, [$scope.item]);
+    }
+
+    /**
+     * Watcher function for the "$scope.item"
+     * It checks if the Case Webform Dropdown is visible
+     */
+    function itemWatcher () {
+      $scope.isCaseWebformDropdownVisible = WebformsCaseAction.isActionAllowed(
+        $scope.webformsAction, [$scope.item], { mode: 'case-details' });
+    }
+  }
 })(angular, CRM.$, CRM._);
