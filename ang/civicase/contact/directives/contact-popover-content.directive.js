f40f9f6d41be972218010cbbce8b3e4c334c2bd8
René Olivo <rene@compucorp.co.uk>
2020-06-11 14:09:49 +0100

    CATL-1394: Move email URL building to controller

--- ang/civicase/contact/directives/contact-popover-content.directive.js
+++ ang/civicase/contact/directives/contact-popover-content.directive.js
@@ -1,4 +1,4 @@
-(function (angular) {
+(function (angular, getCrmUrl) {
   var module = angular.module('civicase');
 
   module.directive('civicaseContactPopoverContent', function () {
@@ -14,5 +14,23 @@
 
   module.controller('civicaseContactPopoverContentController', function ($scope, ContactsCache) {
     $scope.contact = ContactsCache.getCachedContact($scope.contactId);
+    $scope.getEmailUrl = getEmailUrl;
+
+    /**
+     * @returns {string} The URL for composing an email for the current contact.
+     */
+    function getEmailUrl () {
+      var emailUrlParameters = {
+        action: 'add',
+        cid: $scope.contactId,
+        reset: 1
+      };
+
+      if ($scope.caseId) {
+        emailUrlParameters.caseid = $scope.caseId;
+      }
+
+      return getCrmUrl('civicrm/activity/email/add', emailUrlParameters);
+    }
   });
-})(angular);
+})(angular, CRM.url);
