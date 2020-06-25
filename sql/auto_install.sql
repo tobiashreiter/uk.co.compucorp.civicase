5ce928e8ecd7f46275ee07ef0fa5df9177a20f98
Tunbola Ogunwande <tunbolawande@yahoo.com>
2020-06-25 19:06:14 +0100

    RSE-1194: Add the CaseCategoryInstance Entity

--- sql/auto_install.sql
+++ sql/auto_install.sql
@@ -15,3 +15,17 @@ CREATE TABLE `civicase_contactlock` (
   CONSTRAINT FK_civicase_contactlock_case_id FOREIGN KEY (`case_id`) REFERENCES `civicrm_case`(`id`) ON DELETE CASCADE,
   CONSTRAINT FK_civicase_contactlock_contact_id FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `civicrm_case_category_instance`;
+-- /*******************************************************
+-- *
+-- * civicrm_case_category_instance
+-- *
+-- * Stores Case Category Instance Details
+-- *
+-- *******************************************************/
+CREATE TABLE `civicrm_case_category_instance` (
+  `category_id` int unsigned NOT NULL   COMMENT 'One of the values of the case_type_categories option group',
+  `instance_id` int unsigned NOT NULL   COMMENT 'One of the values of the case_category_instance_type option group',
+  UNIQUE INDEX `unique_category`(category_id)
+ ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
