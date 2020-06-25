b28872d00458c552a9f594df4384ef24fb0127a4
Tunbola Ogunwande <tunbolawande@yahoo.com>
2020-06-25 19:53:32 +0100

    RSE-1194: Adds CaseCategoryInstance Support

---
+++ CRM/Civicase/Setup/CaseCategoryInstanceSupport.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Class CRM_Civicase_Setup_CaseCategoryInstanceSupport.
+ */
+class CRM_Civicase_Setup_CaseCategoryInstanceSupport {
+
+  const INSTANCE_OPTION_GROUP = 'case_category_instance_type';
+
+  /**
+   * Adds Case Category Instance Support.
+   */
+  public function apply() {
+    $this->createCaseCategoryInstanceOptionGroup();
+    $this->createDefaultInstanceOptionValue();
+  }
+
+  /**
+   * Create Default Instance Type option value.
+   */
+  private function createDefaultInstanceOptionValue() {
+    CRM_Core_BAO_OptionValue::ensureOptionValueExists(
+      [
+        'option_group_id' => self::INSTANCE_OPTION_GROUP,
+        'name' => 'Default',
+        'value' => 'CRM_Civicase_Service_DefaultInstanceUtils',
+        'label' => 'Default',
+        'is_active' => TRUE,
+        'is_reserved' => TRUE,
+      ]
+    );
+  }
+
+  /**
+   * Create Case Category Instance Type option group.
+   */
+  private function createCaseCategoryInstanceOptionGroup() {
+    CRM_Core_BAO_OptionGroup::ensureOptionGroupExists(
+      [
+        'name' => self::INSTANCE_OPTION_GROUP,
+        'title' => ts('Case Category Instance Type'),
+        'is_reserved' => 1,
+      ]
+    );
+  }
+
+}
--- sql/auto_install.sql
+++ sql/auto_install.sql
@@ -1,4 +1,3 @@
-DROP TABLE IF EXISTS `civicase_contactlock`;
 -- /*******************************************************
 -- *
 -- * civicase_contactlock
@@ -7,7 +6,7 @@ DROP TABLE IF EXISTS `civicase_contactlock`;
 -- * specific cases, to which they will only have basic view access.
 -- *
 -- *******************************************************/
-CREATE TABLE `civicase_contactlock` (
+CREATE TABLE IF NOT EXISTS `civicase_contactlock` (
   `id` int unsigned NOT NULL AUTO_INCREMENT  COMMENT 'Unique CaseContactLock ID',
   `case_id` int unsigned    COMMENT 'Case ID that is locked.',
   `contact_id` int unsigned    COMMENT 'Contact for which the case is locked.' ,
@@ -16,7 +15,6 @@ CREATE TABLE `civicase_contactlock` (
   CONSTRAINT FK_civicase_contactlock_contact_id FOREIGN KEY (`contact_id`) REFERENCES `civicrm_contact`(`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-DROP TABLE IF EXISTS `civicrm_case_category_instance`;
 -- /*******************************************************
 -- *
 -- * civicrm_case_category_instance
@@ -24,7 +22,7 @@ DROP TABLE IF EXISTS `civicrm_case_category_instance`;
 -- * Stores Case Category Instance Details
 -- *
 -- *******************************************************/
-CREATE TABLE `civicrm_case_category_instance` (
+CREATE TABLE IF NOT EXISTS `civicrm_case_category_instance` (
   `category_id` int unsigned NOT NULL   COMMENT 'One of the values of the case_type_categories option group',
   `instance_id` int unsigned NOT NULL   COMMENT 'One of the values of the case_category_instance_type option group',
   UNIQUE INDEX `unique_category`(category_id)
