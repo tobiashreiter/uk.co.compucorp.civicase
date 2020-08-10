a899b779c8212f13c863c32499cc4787d8d879a9
Debarshi Bhaumik <debarshi@compucorp.co.uk>
2020-08-10 09:04:33 +0100

    CATL-1570: Update unit tests

--- ang/test/civicase/case/contact-case-tab/directives/contact-case-tab-case-details.directive.spec.js
+++ ang/test/civicase/case/contact-case-tab/directives/contact-case-tab-case-details.directive.spec.js
@@ -26,7 +26,7 @@
         mockCase['case_type_id.case_type_category'] = caseTypeCategory.value;
         expectedUrl = '/civicrm/case/a/' +
           `?case_type_category=${caseTypeCategory.name}` +
-          `#/case/list?caseId=${mockCase.id}`;
+          `#/case/list?caseId=${mockCase.id}&focus=1&sas=1`;
         returnedUrl = $scope.getCaseDetailsUrl(mockCase);
       });
 
