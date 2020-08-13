ad08c65ef16a81b275f988f9c772186ce68da6f7
Tunbola Ogunwande <tunbolawande@yahoo.com>
2020-08-13 11:22:05 +0100

    CATL-1637: Add Fabricators and Unit Test Helpers

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -4,6 +4,16 @@ ini_set('memory_limit', '2G');
 ini_set('safe_mode', 0);
 eval(cv('php:boot --level=classloader', 'phpcode'));
 
+// Allow autoloading of PHPUnit helper classes in this extension.
+$loader = new \Composer\Autoload\ClassLoader();
+$loader->add('CRM_', __DIR__);
+$loader->add('Civi\\', __DIR__);
+$loader->add('api_', __DIR__);
+$loader->add('api\\', __DIR__);
+$loader->register();
+
+require_once 'BaseHeadlessTest.php';
+
 /**
  * Call the "cv" command.
  *
