460a4769065e419a366c274506b8d3b86afb1bc5
René Olivo <rene@compucorp.co.uk>
2020-08-21 05:07:44 +0100

    CATL-1588: Handle date change scenario

--- ang/civicase/case/details/people-tab/services/remove-date-picker-hrefs.service.js
+++ ang/civicase/case/details/people-tab/services/remove-date-picker-hrefs.service.js
@@ -1,24 +1,24 @@
-(function (angular, $) {
+(function (angular, $, _) {
   var module = angular.module('civicase');
 
   module.service('removeDatePickerHrefs', function ($timeout) {
     var HREFS_SELECTOR = '[data-handler="selectDay"] a';
 
-    return datepickerBeforeShow;
+    return removeDatePickerHrefs;
 
     /**
      * Removes HREF attribtues from anchor elements defined in calendar inputs.
      * This is done to avoid switching the angular route's path unintentionally.
-     *
-     * @param {object} inputElement A reference to the date picker's input element.
-     * @param {object} uiObject The jQuery UI data object.
      */
-    function datepickerBeforeShow (inputElement, uiObject) {
-      setTimeout(function () {
+    function removeDatePickerHrefs () {
+      // Note: the last argument is always the UI Object
+      var uiObject = _.last(arguments);
+
+      $timeout(function () {
         uiObject.dpDiv
           .find(HREFS_SELECTOR)
           .removeAttr('href');
       });
     }
   });
-})(angular, CRM.$);
+})(angular, CRM.$, CRM._);
