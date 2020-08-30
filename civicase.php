25f193b54920e6ec565637e60d48ff2099792ffa
shahrukh-compucorp <shahrukh@compucorp.co.uk>
2020-08-30 17:07:33 +0500

    CATL-1684: New hook added to make a global redirect if a corresponding web form is available

--- civicase.php
+++ civicase.php
@@ -211,6 +211,7 @@ function civicase_civicrm_buildForm($formName, &$form) {
     new CRM_Civicase_Hook_BuildForm_AddStyleFieldToCaseCustomGroups(),
     new CRM_Civicase_Hook_BuildForm_DisplayAllCustomGroupsInCaseForm(),
     new CRM_Civicase_Hook_BuildForm_LinkToCaseSearchByCaseId(),
+    new CRM_Civicase_Hook_BuildForm_CaseTypeCategoryWebFormRedirect(),
   ];
 
   foreach ($hooks as $hook) {
