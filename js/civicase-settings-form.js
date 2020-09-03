8fb18522368dfb74266898f48543f4a722964bd8
shahrukh-compucorp <shahrukh@compucorp.co.uk>
2020-09-03 18:57:21 +0500

    CATL-1674: Hide single case role setting if multiple clients per case are allowed

--- js/civicase-settings-form.js
+++ js/civicase-settings-form.js
@@ -1,18 +1,23 @@
 (function ($) {
   $(document).on('crmLoad', function () {
-    var $elementsThatToggleVisibility = $('[data-toggles-visibility-for]');
+    var $radioButtonsThatToggleVisibility = $('[data-toggles-visibility-for]');
+    var $multiCaseClient = $('#civicaseAllowMultipleClients');
 
     (function init () {
-      toggleVisibility();
+      toggleVisibilityOnRadioButtonChange();
+      toggleVisibilityForSingleCaseRole();
 
-      $elementsThatToggleVisibility.change(toggleVisibility);
+      $radioButtonsThatToggleVisibility.change(toggleVisibilityOnRadioButtonChange);
+      if ($multiCaseClient.length) {
+        $multiCaseClient.change(toggleVisibilityForSingleCaseRole);
+      }
     })();
 
     /**
-     * Toggles the visibility of civicase settings fields based on value.
+     * Toggles the visibility of civicase settings fields based on radio button values.
      */
-    function toggleVisibility () {
-      $elementsThatToggleVisibility.filter(':checked')
+    function toggleVisibilityOnRadioButtonChange () {
+      $radioButtonsThatToggleVisibility.filter(':checked')
         .each(function () {
           var $element = $(this);
           var isAllowed = $element.val() === '1';
@@ -22,5 +27,40 @@
           isAllowed ? $elementToToggle.show() : $elementToToggle.hide();
         });
     }
+
+    /**
+     * Toggles the visibility of single case role.
+     */
+    function toggleVisibilityForSingleCaseRole () {
+      var $defaultMultiCaseClientValue = $('.CRM_Admin_Form_Setting_Case').attr('defaultMultipleCaseClient') ?
+        $('.CRM_Admin_Form_Setting_Case').attr('defaultMultipleCaseClient') :
+        0;
+      $multiCaseClient.val() == 0 ||
+      ($multiCaseClient.val().toLowerCase() == 'default' && $defaultMultiCaseClientValue == 0) ?
+        showSingleCaseRole() :
+        hideSingleCaseRole();
+    }
+
+    /**
+     * Shows single case role.
+     */
+    function showSingleCaseRole () {
+      if ($('.crm-mail-form-block-civicaseSingleCaseRolePerType').length) {
+        $('.crm-mail-form-block-civicaseSingleCaseRolePerType').show();
+      }
+    }
+
+    /**
+     * Hides single case role.
+     */
+    function hideSingleCaseRole () {
+      if ($('.crm-mail-form-block-civicaseSingleCaseRolePerType').length) {
+        $('.crm-mail-form-block-civicaseSingleCaseRolePerType').hide();
+      }
+      if ($('#civicaseSingleCaseRolePerType_civicaseSingleCaseRolePerType').length) {
+        $('#civicaseSingleCaseRolePerType_civicaseSingleCaseRolePerType').prop("checked", false);
+      }
+    }
+
   });
 })(CRM.$);
