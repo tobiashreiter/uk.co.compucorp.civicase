33df27fe6e9a9bfc4bbdf6b0d4609e18e8f4adcc
Mathieu Lutfy <mathieu@symbiotic.coop>
2020-10-09 16:34:10 -0400

    Contact Case Tab: fix Case link

--- ang/test/civicase/case/contact-case-tab/directives/contact-case-tab-case-details.directive.spec.js
+++ ang/test/civicase/case/contact-case-tab/directives/contact-case-tab-case-details.directive.spec.js
@@ -22,9 +22,9 @@
           .sample()
           .value();
         mockCase['case_type_id.case_type_category'] = caseTypeCategory.value;
-        expectedUrl = '/civicrm/case/a/' +
+        expectedUrl = CRM.url('/civicrm/case/a/',
           `?case_type_category=${caseTypeCategory.name}` +
-          `#/case/list?caseId=${mockCase.id}&focus=1&all_statuses=1`;
+          `#/case/list?caseId=${mockCase.id}&focus=1&all_statuses=1`);
         returnedUrl = $scope.getCaseDetailsUrl(mockCase);
       });
 
