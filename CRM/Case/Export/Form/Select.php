8d78671223aa9a94bd8577998c7b0c2ef213d844
Tano Rojas <lisandro@compucorp.co.uk>
2021-05-31 21:36:45 +0700

    ACU-594: Add Map and Select classes for exporting cases

---
+++ CRM/Case/Export/Form/Select.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Class added for exporting cases.
+ *
+ * Added temporarily for solving an incompatibility issue while exporting
+ * cases in standalone window.
+ */
+class CRM_Case_Export_Form_Select extends CRM_Export_Form_Select_Case {
+}
