5e4434bed1746b07d25bf09bc80a168661955c14
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-02-27 17:42:24 +0100

    BTHAB-23: Refactor method to retrieve feature case instance

--- CRM/Civicase/Hook/NavigationMenu/CaseInstanceFeaturesMenu.php
+++ CRM/Civicase/Hook/NavigationMenu/CaseInstanceFeaturesMenu.php
@@ -1,8 +1,7 @@
 <?php
 
 use CRM_Certificate_ExtensionUtil as E;
-use Civi\Api4\CaseCategoryFeatures;
-use Civi\Api4\OptionGroup;
+use CRM_Civicase_Service_CaseTypeCategoryFeatures as CaseTypeCategoryFeatures;
 
 /**
  * Add menu for enabled features on Case instnace menus.
@@ -36,8 +35,8 @@ class CRM_Civicase_Hook_NavigationMenu_CaseInstanceFeaturesMenu extends CRM_Civi
    */
   private function addFeaturesMenu(array &$menu) {
     try {
-
-      $caseInstancesGroup = $this->retrieveCaseInstanceWithEnabledFeatures();
+      $caseTypeCategoryFeatures = new CaseTypeCategoryFeatures();
+      $caseInstancesGroup = $caseTypeCategoryFeatures->retrieveCaseInstanceWithEnabledFeatures(self::FEATURES_WITH_MENU);
 
       foreach ($caseInstancesGroup as $caseInstances) {
         $separator = 0;
@@ -71,42 +70,6 @@ class CRM_Civicase_Hook_NavigationMenu_CaseInstanceFeaturesMenu extends CRM_Civi
         ],
       ]);
     }
-  }
-
-  /**
-   * Retrieves case instance that has the defined features enabled.
-   *
-   * @return array
-   *   Array of Key\Pair value grouped by case instance id.
-   */
-  private function retrieveCaseInstanceWithEnabledFeatures() {
-    $caseInstanceGroup = OptionGroup::get()->addWhere('name', '=', 'case_type_categories')->execute()[0] ?? NULL;
-
-    if (empty($caseInstanceGroup)) {
-      return [];
-    }
-
-    $result = CaseCategoryFeatures::get()
-      ->addSelect('*', 'option_value.label', 'option_value.name', 'feature_id:name', 'feature_id:label', 'navigation.id')
-      ->addJoin('OptionValue AS option_value', 'LEFT',
-      ['option_value.value', '=', 'category_id']
-    )
-      ->addJoin('Navigation AS navigation', 'LEFT',
-      ['navigation.name', '=', 'option_value.name']
-    )
-      ->addWhere('option_value.option_group_id', '=', $caseInstanceGroup['id'])
-      ->addWhere('feature_id:name', 'IN', self::FEATURES_WITH_MENU)
-      ->execute();
-
-    $caseCategoriesGroup = array_reduce((array) $result, function (array $accumulator, array $element) {
-      $accumulator[$element['category_id']]['items'][] = $element;
-      $accumulator[$element['category_id']]['navigation_id'] = $element['navigation.id'];
-      $accumulator[$element['category_id']]['name'] = $element['option_value.name'];
-
-      return $accumulator;
-    }, []);
-
-    return $caseCategoriesGroup;
   }
 
 }
--- CRM/Civicase/Service/CaseTypeCategoryFeatures.php
+++ CRM/Civicase/Service/CaseTypeCategoryFeatures.php
@@ -1,5 +1,7 @@
 <?php
 
+use Civi\Api4\OptionGroup;
+use Civi\Api4\CaseCategoryFeatures;
 use Civi\Api4\OptionValue as OptionValue;
 
 /**
@@ -19,6 +21,45 @@ class CRM_Civicase_Service_CaseTypeCategoryFeatures {
       ->execute();
 
     return $optionValues;
+  }
+
+  /**
+   * Retrieves case instance that has the defined features enabled.
+   *
+   * @param array $features
+   *   The features to retrieve case instances for.
+   *
+   * @return array
+   *   Array of Key\Pair value grouped by case instance id.
+   */
+  public function retrieveCaseInstanceWithEnabledFeatures(array $features) {
+    $caseInstanceGroup = OptionGroup::get()->addWhere('name', '=', 'case_type_categories')->execute()[0] ?? NULL;
+
+    if (empty($caseInstanceGroup)) {
+      return [];
+    }
+
+    $result = CaseCategoryFeatures::get()
+      ->addSelect('*', 'option_value.label', 'option_value.name', 'feature_id:name', 'feature_id:label', 'navigation.id')
+      ->addJoin('OptionValue AS option_value', 'LEFT',
+      ['option_value.value', '=', 'category_id']
+    )
+      ->addJoin('Navigation AS navigation', 'LEFT',
+      ['navigation.name', '=', 'option_value.name']
+    )
+      ->addWhere('option_value.option_group_id', '=', $caseInstanceGroup['id'])
+      ->addWhere('feature_id:name', 'IN', $features)
+      ->execute();
+
+    $caseCategoriesGroup = array_reduce((array) $result, function (array $accumulator, array $element) {
+      $accumulator[$element['category_id']]['items'][] = $element;
+      $accumulator[$element['category_id']]['navigation_id'] = $element['navigation.id'];
+      $accumulator[$element['category_id']]['name'] = $element['option_value.name'];
+
+      return $accumulator;
+    }, []);
+
+    return $caseCategoriesGroup;
   }
 
 }
