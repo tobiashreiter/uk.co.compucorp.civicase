a480f08bfb5fd614207432c29c773f00b52bd984
olayiwola-compucorp <olayiwola@compucorp.co.uk>
2023-04-03 19:52:20 +0100

    BTHAB-34: Add more test case scenarios to the salesOrder lin item generator test
    Here we ensure the appropraite number of line items with the expected values are generated

--- tests/phpunit/CRM/Civicase/Service/CaseSalesOrderLineItemsGeneratorTest.php
+++ tests/phpunit/CRM/Civicase/Service/CaseSalesOrderLineItemsGeneratorTest.php
@@ -4,7 +4,7 @@ use Civi\Api4\CaseSalesOrder;
 use CRM_Civicase_Service_CaseSalesOrderLineItemsGenerator as SalesOrderService;
 
 /**
- * Runs tests on CRM_Civicase_Service_CaseSalesOrderLineItemsGenerator Service tests.
+ * Runs tests on CRM_Civicase_Service_CaseSalesOrderLineItemsGenerator tests.
  *
  * @group headless
  */
@@ -20,7 +20,7 @@ class CRM_Civicase_Service_CaseSalesOrderLineItemsGeneratorsTest extends BaseHea
   }
 
   /**
-   * Ensures the correct number of line item is created.
+   * Ensures the correct number of line item is generated.
    *
    * When there's no previous contribution.
    */
@@ -34,7 +34,7 @@ class CRM_Civicase_Service_CaseSalesOrderLineItemsGeneratorsTest extends BaseHea
   }
 
   /**
-   * Ensures the correct number of line item is created.
+   * Ensures the correct number of line item is generated.
    *
    * When there's previous contribution.
    */
@@ -44,7 +44,7 @@ class CRM_Civicase_Service_CaseSalesOrderLineItemsGeneratorsTest extends BaseHea
     $previousContributionCount = rand(1, 4);
     for ($i = 0; $i < $previousContributionCount; $i++) {
       CaseSalesOrder::contributionCreateAction()
-        ->setIds([$salesOrder['id']])
+        ->setSalesOrderIds([$salesOrder['id']])
         ->setStatusId(1)
         ->setToBeInvoiced(SalesOrderService::INVOICE_PERCENT)
         ->setPercentValue(20)
@@ -57,6 +57,65 @@ class CRM_Civicase_Service_CaseSalesOrderLineItemsGeneratorsTest extends BaseHea
     $lineItems = $salesOrderService->generateLineItems();
 
     $this->assertCount(($previousContributionCount * 2) + 2, $lineItems);
+  }
+
+  /**
+   * Ensures the correct number of line item is generated.
+   *
+   * When there's discount with the right value.
+   */
+  public function testCorrectNumberOfLineItemsIsGeneratedWithDiscount() {
+    $percent = 20;
+    $salesOrder = $this->getCaseSalesOrderData();
+    $salesOrder['items'][] = $this->getCaseSalesOrderLineData(['discounted_percentage' => $percent]);
+    $salesOrder['id'] = CaseSalesOrder::save()
+      ->addRecord($salesOrder)
+      ->execute()
+      ->jsonSerialize()[0]['id'];
+
+    $salesOrderService = new SalesOrderService($salesOrder['id'], SalesOrderService::INVOICE_REMAIN, 0);
+    $lineItems = $salesOrderService->generateLineItems();
+
+    usort($lineItems, fn($a, $b) => $a['line_total'] <=> $b['line_total']);
+
+    $this->assertCount(2, $lineItems);
+    $this->assertEquals(-1 * $salesOrder['items'][0]['subtotal_amount'] * $percent / 100, $lineItems[0]['line_total']);
+  }
+
+  /**
+   * Ensures the correct number of line item is generated.
+   *
+   * When the discount value is zero and the value is as expected.
+   */
+  public function testCorrectNumberOfLineItemsIsGeneratedWhenDiscountIsZero() {
+    $percent = 0;
+    $salesOrder = $this->getCaseSalesOrderData();
+    $salesOrder['items'][] = $this->getCaseSalesOrderLineData(['discounted_percentage' => $percent]);
+    $salesOrder['id'] = CaseSalesOrder::save()
+      ->addRecord($salesOrder)
+      ->execute()
+      ->jsonSerialize()[0]['id'];
+
+    $salesOrderService = new SalesOrderService($salesOrder['id'], SalesOrderService::INVOICE_REMAIN, 0);
+    $lineItems = $salesOrderService->generateLineItems();
+
+    $this->assertCount(1, $lineItems);
+    $this->assertEquals($salesOrder['items'][0]['subtotal_amount'], $lineItems[0]['line_total']);
+  }
+
+  /**
+   * Ensures the value of the generated line item is correct.
+   */
+  public function testGeneratedPercentLineItemHasTheAppropraiteValue() {
+    $percent = rand(20, 40);
+    $salesOrder = $this->createCaseSalesOrder();
+
+    $salesOrderService = new SalesOrderService($salesOrder['id'], SalesOrderService::INVOICE_PERCENT, $percent);
+    $lineItems = $salesOrderService->generateLineItems();
+
+    $this->assertCount(2, $lineItems);
+    $this->assertEquals($salesOrder['items'][0]['subtotal_amount'] * $percent / 100, $lineItems[0]['line_total']);
+    $this->assertEquals($salesOrder['items'][1]['subtotal_amount'] * $percent / 100, $lineItems[1]['line_total']);
   }
 
 }
