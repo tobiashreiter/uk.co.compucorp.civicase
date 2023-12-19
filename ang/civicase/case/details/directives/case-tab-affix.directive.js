7acfe13dcbbd4cac0c0970e91e0ab0af0e316d1a
Toby Reiter <reitert@si.edu>
2023-12-19 12:03:16 -0500

    CiviCase Extension
    Fixed affix functionality, broken due to Bootstrap upgrade.

--- ang/civicase/case/details/directives/case-tab-affix.directive.js
+++ ang/civicase/case/details/directives/case-tab-affix.directive.js
@@ -1,63 +1,95 @@
-(function (angular, $, _) {
-  var module = angular.module('civicase');
+(function(angular, $, _) {
+    var module = angular.module('civicase');
 
-  module.directive('civicaseCaseTabAffix', function ($rootScope, $timeout) {
-    return {
-      link: civicaseCaseTabAffixLink
-    };
+    module.directive('civicaseCaseTabAffix', function($rootScope, $timeout) {
+        return {
+            link: civicaseCaseTabAffixLink
+        };
 
-    /**
-     * Link function for civicaseCaseTabAffix
-     *
-     * @param {object} scope scope object
-     * @param {object} $el element
-     * @param {object} attrs attributes
-     */
-    function civicaseCaseTabAffixLink (scope, $el, attrs) {
-      var $caseNavigation = $('.civicase__case-body_tab');
-      var $toolbarDrawer = $('#toolbar');
-      var $casePanelBody = $('.civicase__case-details-panel > .panel-body');
-      var bodyPadding = parseInt($('body').css('padding-top'), 10); // to see the space for fixed menus
+        /**
+         * Link function for civicaseCaseTabAffix
+         *
+         * @param {object} scope scope object
+         * @param {object} $el element
+         * @param {object} attrs attributes
+         */
+        function civicaseCaseTabAffixLink(scope, $el, attrs) {
+            var $caseNavigation = $('.civicase__case-body_tab');
 
-      (function init () {
-        caseNavAffix();
-        initWatchers();
-      }());
+            var $element = $caseNavigation;
 
-      /**
-       * Affix Case Nav
-       */
-      function caseNavAffix () {
-        $timeout(function () {
-          $caseNavigation.affix({
-            offset: {
-              top: $casePanelBody.offset().top - bodyPadding
+            function affixElementExtras() {
+                $element.css('width','100%');
             }
-          }).on('affixed.bs.affix', function () {
-            var caseNavigationTopPosition = $toolbarDrawer.is(':visible')
-              ? $toolbarDrawer.height()
-              : bodyPadding;
 
-            $caseNavigation.css('top', caseNavigationTopPosition);
-          }).on('affixed-top.bs.affix', function () {
-            $caseNavigation.css('top', 'auto');
-          });
-        });
-      }
-
-      /**
-       * Init watchers
-       */
-      function initWatchers () {
-        scope.$on('civicase::case-search::dropdown-toggle', function () {
-          $timeout(function () {
-            // Reset right case view tab header
-            if ($caseNavigation.data('bs.affix')) {
-              $caseNavigation.data('bs.affix').options.offset.top = $casePanelBody.offset().top - bodyPadding;
+            function unAffixElementExtras() {
+                return;
             }
-          });
-        });
-      }
-    }
-  });
+
+            function affixElement() {
+                if (!$element.data('affixed')) {
+                    $element.data('affixed', true);
+                    var bodyPadding = parseInt($('body').css('padding-top'), 10); // to see the space for fixed menus
+                    $element.css('position', 'fixed');
+                    $element.css('top', bodyPadding + 'px');
+                    $element.data('originalOffsetLeft', $element.offset().left);
+                    $element.css('left', 0);
+                    $element.data('originalZIndex', $element.css("z-index"));
+                    $element.css('z-index', 10000);
+                    affixElementExtras();
+                }
+            }
+
+            function unAffixElement() {
+                if ($element.data('affixed')) {
+                    $element.removeData('affixed');
+                    $element.css('position', 'static');
+                    var originalOffsetTop = $element.data('originalOffsetLeft');
+                    $element.css('top', originalOffsetTop + 'px');
+                    var originalOffsetLeft = $element.data('originalOffsetTop');
+                    $element.css('left', originalOffsetLeft + 'px');
+                    var originalZIndex = $element.data('originalZIndex');
+                    $element.css('z-index', originalZIndex);
+                    unAffixElementExtras();
+                }
+            }
+
+            var delay = (function() {
+                var timer = 0;
+                return function(callback, ms) {
+                    clearTimeout(timer);
+                    timer = setTimeout(callback, ms);
+                };
+            })();
+            $(document).scroll(function() {
+                delay(function() {
+                    checkForAffixingElement();
+                }, 10);
+            });
+
+            function checkForAffixingElement() {
+                if (!$element.data('originalOffsetTop')) {
+                    $element.data('originalOffsetTop', $element.offset().top);
+                    $element.data('originalHeight', $element.height());
+                }
+
+                // If the header is currently affixed, then that means we need to subtract the height of the header from the offset top to get the offset we should be checking
+                if ($element.data('affixed')) {
+                    var offsetTopCheck = $element.data('originalOffsetTop') - $element.data('originalHeight');
+                }
+                else {
+                    var offsetTopCheck = $element.data('originalOffsetTop');
+                }
+
+                var windowOffsetTop = window.scrollY;
+
+                if (offsetTopCheck < windowOffsetTop) {
+                    affixElement();
+                }
+                else {
+                    unAffixElement();
+                }
+            }
+        }
+    });
 })(angular, CRM.$, CRM._);
--- ang/civicase/case/list/directives/sticky-table-header.directive.js
+++ ang/civicase/case/list/directives/sticky-table-header.directive.js
@@ -1,125 +1,105 @@
-(function (angular) {
-  var module = angular.module('civicase');
-  var $ = angular.element;
-
-  /**
-   * Directive for the sticky table header functionality on case list page
-   */
-  module.directive('civicaseStickyTableHeader', function ($rootScope, $timeout) {
-    return {
-      restrict: 'A',
-      link: civicaseStickyTableHeaderLink
-    };
+(function(angular) {
+    var module = angular.module('civicase');
+    var $ = angular.element;
 
     /**
-     * Link function for stickyTableHeader Directive
-     *
-     * @param {object} scope
-     *   Scope under which directive is called
-     * @param {object} $el
-     *   Element on which directive is called
-     * @param {object} attrs
-     *   attributes of directive
+     * Directive for the sticky table header functionality on case list page
      */
-    function civicaseStickyTableHeaderLink (scope, $el, attrs) {
-      var $toolbarDrawer = $('#toolbar');
-      var $table = $el;
-      var $header = $el.find('thead');
+    module.directive('civicaseStickyTableHeader', function($rootScope, $timeout) {
+        return {
+            restrict: 'A',
+            link: civicaseStickyTableHeaderLink
+        };
 
-      (function init () {
-        initWatchers();
-        initSubscribers();
-      }());
+        /**
+         * Link function for stickyTableHeader Directive
+         *
+         * @param {object} scope
+         *   Scope under which directive is called
+         * @param {object} $el
+         *   Element on which directive is called
+         * @param {object} attrs
+         *   attributes of directive
+         */
+        function civicaseStickyTableHeaderLink(scope, $el, attrs) {
+            var $header = $el.find('thead');
+            var $element = $header;
 
-      /**
-       * Initialise all watchers
-       */
-      function initWatchers () {
-        scope.$watch('isLoading', fixPositioning);
-        scope.$watch('caseIsFocused', fixPositioning);
-        scope.$watch('viewingCase', fixPositioning);
-      }
-
-      /**
-       * Initialise all subscribers
-       */
-      function initSubscribers () {
-        scope.$on('civicase::case-search::dropdown-toggle', reAdjustFixedHeader);
-        $rootScope.$on('civicase::bulk-actions::bulk-message-toggle', reAdjustFixedHeader);
-      }
-
-      /**
-       * Loads only if loading completes and case is not focused and
-       * not viewing the case details for fixed header
-       */
-      function fixPositioning () {
-        if (!scope.loading && !scope.caseIsFocused && !scope.viewingCase) {
-          affixTableHeader();
-        }
-      }
-
-      /**
-       * Checks if the affix header is already affixed,
-       * if yes, readjusts it,
-       * otherwise inits the affix
-       */
-      function affixTableHeader () {
-        if ($header.data('bs.affix')) {
-          reAdjustFixedHeader();
-        } else {
-          computeFixPositioning();
-        }
-      }
-
-      /**
-       * Assign min-width values to th to have solid grid
-       * Timeout if 0s added to execute logic after DOM repainting completes
-       */
-      function computeFixPositioning () {
-        $timeout(function () {
-          var bodyPadding = parseInt($('body').css('padding-top'), 10); // to see the space for fixed menus
-          var topPos = $header.offset().top - bodyPadding;
-          $('th', $header).each(function () {
-            $(this).css('min-width', $(this).outerWidth() + 'px');
-          });
-
-          // Define when to make the element sticky (affixed)
-          $header.affix({
-            offset: {
-              top: topPos
+            function affixElementExtras() {
+                $('th', $element).each(function() {
+                    $(this).css('min-width', $(this).outerWidth() + 'px');
+                });
+                $element.find('th:first').css('position', 'static');
             }
-          })
-          // After element is affixed set scrolling pos (to avoid glitch) and top position
-            .on('affixed.bs.affix', function () {
-              $header.scrollLeft($table.scrollLeft()); // Bind scrolling
 
-              $header.css('top', bodyPadding + 'px'); // Set top pos to body padding so that it don't overlap with the toolbar
-              $toolbarDrawer.is(':visible') && $table.css('padding-top', $header.height() + 'px'); // Add top padding to remove the glitch when header moves out of DOM relative position
-            })
-            .on('affixed-top.bs.affix', function () {
-              $header.css('top', 0); // Resets top pos when in default state
-              $table.css('padding-top', 0); // Resets padding top when in default state
+            function unAffixElementExtras() {
+                $element.find('th:first').css('position', 'absolute');
+            }
+
+            function affixElement() {
+                if (!$element.data('affixed')) {
+                    $element.data('affixed', true);
+                    var bodyPadding = parseInt($('body').css('padding-top'), 10); // to see the space for fixed menus
+                    $element.css('position', 'fixed');
+                    $element.css('top', bodyPadding + 'px');
+                    $element.data('originalOffsetLeft', $element.offset().left);
+                    $element.css('left', 0);
+                    $element.data('originalZIndex', $element.css("z-index"));
+                    $element.css('z-index', 10000);
+                    affixElementExtras();
+                }
+            }
+
+            function unAffixElement() {
+                if ($element.data('affixed')) {
+                    $element.removeData('affixed');
+                    $element.css('position', 'static');
+                    var originalOffsetTop = $element.data('originalOffsetLeft');
+                    $element.css('top', originalOffsetTop + 'px');
+                    var originalOffsetLeft = $element.data('originalOffsetTop');
+                    $element.css('left', originalOffsetLeft + 'px');
+                    var originalZIndex = $element.data('originalZIndex');
+                    $element.css('z-index', originalZIndex);
+                    unAffixElementExtras();
+                }
+            }
+
+            var delay = (function() {
+                var timer = 0;
+                return function(callback, ms) {
+                    clearTimeout(timer);
+                    timer = setTimeout(callback, ms);
+                };
+            })();
+            $(document).scroll(function() {
+                delay(function() {
+                    checkForAffixingElement();
+                }, 10);
             });
 
-          // Attach scroll function
-          $table.scroll(function () {
-            $header.scrollLeft($(this).scrollLeft());
-          });
-        }, 0);
-      }
+            function checkForAffixingElement() {
+                if (!$element.data('originalOffsetTop')) {
+                    $element.data('originalOffsetTop', $element.offset().top);
+                    $element.data('originalHeight', $element.height());
+                }
 
-      /**
-       * Subscriber for fixed header
-       */
-      function reAdjustFixedHeader () {
-        $timeout(function () {
-          var bodyPadding = parseInt($('body').css('padding-top'), 10); // to see the space for fixed menus
+                // If the header is currently affixed, then that means we need to subtract the height of the header from the offset top to get the offset we should be checking
+                if ($element.data('affixed')) {
+                    var offsetTopCheck = $element.data('originalOffsetTop') - $element.data('originalHeight');
+                }
+                else {
+                    var offsetTopCheck = $element.data('originalOffsetTop');
+                }
 
-          if ($header.data('bs.affix')) {
-            $header.data('bs.affix').options.offset.top = $header.offset().top - bodyPadding;
-          }
-        });
-      }
-    }
-  });
+                var windowOffsetTop = window.scrollY;
+
+                if (offsetTopCheck < windowOffsetTop) {
+                    affixElement();
+                }
+                else {
+                    unAffixElement();
+                }
+            }
+        }
+    });
 })(angular);
