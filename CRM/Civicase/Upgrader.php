16c49b2caa0654aff99b32464b1cf4f7ad626099
Mathieu Lu <mathieu@symbiotic.coop>
2024-04-16 10:24:27 -0400

    Upgrade civix, bump min-req CiviCRM 5.65

--- CRM/Civicase/Upgrader.php
+++ CRM/Civicase/Upgrader.php
@@ -19,7 +19,7 @@ use CRM_Civicase_Setup_AddMyActivitiesMenu as AddMyActivitiesMenu;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Civicase_Upgrader extends CRM_Civicase_Upgrader_Base {
+class CRM_Civicase_Upgrader extends CRM_Extension_Upgrader_Base {
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
--- civicase.php
+++ civicase.php
@@ -82,15 +82,6 @@ function civicase_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function civicase_civicrm_xmlMenu(&$files) {
-  _civicase_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -100,85 +91,12 @@ function civicase_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function civicase_civicrm_postInstall() {
-  _civicase_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function civicase_civicrm_uninstall() {
-  _civicase_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function civicase_civicrm_enable() {
   _civicase_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function civicase_civicrm_disable() {
-  _civicase_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function civicase_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _civicase_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function civicase_civicrm_managed(&$entities) {
-  _civicase_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function civicase_civicrm_caseTypes(&$caseTypes) {
-  _civicase_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function civicase_civicrm_angularModules(&$angularModules) {
-  _civicase_civix_civicrm_angularModules($angularModules);
 }
 
 /**
@@ -194,15 +112,6 @@ function civicase_civicrm_alterMenu(&$items) {
   $items['civicrm/activity/pdf/add']['ids_arguments']['json'][] = 'civicase_reload';
   $items['civicrm/case/cd/edit']['ids_arguments']['json'][] = 'civicase_reload';
   $items['civicrm/export/standalone']['ids_arguments']['json'][] = 'civicase_reload';
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function civicase_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _civicase_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
@@ -489,7 +398,6 @@ function civicase_civicrm_selectWhereClause($entity, &$clauses) {
  * Implements hook_civicrm_entityTypes().
  */
 function civicase_civicrm_entityTypes(&$entityTypes) {
-  _civicase_civix_civicrm_entityTypes($entityTypes);
   _civicase_add_case_category_case_type_entity($entityTypes);
 }
 
